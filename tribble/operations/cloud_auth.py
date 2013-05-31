3f1029ff316ffa568f891bdab2dfd05093bba6e4
cloudnull <kevin.carter@rackspace.com>
2013-05-30 22:37:33 -0500

    better authentication for multicloud, now using the latest libcloud installation.

--- tribble/operations/cloud_auth.py
+++ tribble/operations/cloud_auth.py
@@ -16,10 +16,7 @@ def apiauth(packet):
     """
     security.CA_CERTS_PATH.append('dist/cacert.pem')
 
-    endpoints = {'ORD': Provider.RACKSPACE_NOVA_ORD,
-                 'DFW': Provider.RACKSPACE_NOVA_DFW,
-                 'LON': Provider.RACKSPACE_NOVA_LON,
-                 'AP_NORTHEAST': Provider.EC2_AP_NORTHEAST,
+    endpoints = {'AP_NORTHEAST': Provider.EC2_AP_NORTHEAST,
                  'AP_SOUTHEAST': Provider.EC2_AP_SOUTHEAST,
                  'EU_WEST': Provider.EC2_EU_WEST,
                  'US_EAST': Provider.EC2_US_EAST,
@@ -33,18 +30,27 @@ def apiauth(packet):
                      'ex_force_auth_version': packet.get('cloud_version',
                                                          '2.0_password'),
                      'ex_tenant_name': packet.get('cloud_username')}
+        elif provider.upper() == 'RACKSPACE':
+            driver = get_driver(Provider.RACKSPACE)
+            specs = {'ex_force_auth_url': packet.get('cloud_url'),
+                     'ex_force_auth_version': packet.get('cloud_version'),
+                     'datacenter': packet.get('cloud_region').lower()}
         elif provider.upper() == 'VMWARE':
             driver = get_driver(Provider.VCLOUD)
             specs = {'host': packet.get('cloud_url'),
                      'api_version': packet.get('cloud_version', '1.5')}
-        elif packet.get('cloud_region').upper() in endpoints:
-            region = packet.get('cloud_region').upper()
-            driver = get_driver(endpoints[region])
-            specs = {'ex_force_auth_url': packet.get('cloud_url'),
-                     'ex_force_auth_version': packet.get('cloud_version')}
+        elif provider.upper() == 'AMAZON':
+            if packet.get('cloud_region').upper() in endpoints:
+                region = packet.get('cloud_region').upper()
+                driver = get_driver(endpoints[region])
+                specs = {'ex_force_auth_url': packet.get('cloud_url'),
+                         'ex_force_auth_version': packet.get('cloud_version')}
+            else:
+                raise CantContinue('No Region Found.')
         elif packet.get('cloud_provider') in Provider.__dict__:
             region = packet.get('cloud_region').upper()
-            driver = get_driver(Provider.__dict__[region])
+            _provider = Provider.__dict__[packet.get('cloud_provider')]
+            driver = get_driver(_provider)
             specs = {'ex_force_auth_url': packet.get('cloud_url'),
                      'ex_force_auth_version': packet.get('cloud_version')}
         else:
